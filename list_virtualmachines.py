dba5e122615f4c7bb665a75d67203c36f3037fbf
adrian.j.powell <adrian.j.powell@bjss.com>
2022-08-09 19:08:38 +0100

    Updates to get vm status

--- list_virtualmachines.py
+++ list_virtualmachines.py
@@ -3,6 +3,7 @@
 # Import the needed credential and management objects from the libraries.
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.compute import ComputeManagementClient
 import os
 
 # Acquire a credential object using CLI-based authentication.
@@ -17,6 +18,9 @@ resource_group = os.getenv("RESOURCE_GROUP_NAME", "test-cluster-rg")
 # Obtain the management object for resources.
 resource_client = ResourceManagementClient(credential, subscription_id)
 
+# Initialize the compute management client with the RunAs credential and specify the subscription to work against.
+compute_client = ComputeManagementClient(credential, subscription_id)
+
 # Retrieve the list of resources in "myResourceGroup" (change to any name desired).
 # The expand argument includes additional properties in the output.
 resource_list = resource_client.resources.list_by_resource_group(
@@ -27,15 +31,22 @@ column_width = 36
 
 print('')
 print("Resource".ljust(column_width) + "Type".ljust(column_width)
-      + "Create date".ljust(column_width)  # + "Change date".ljust(column_width)
+      + "Create date".ljust(column_width)  + "VM Status".ljust(column_width)
       )
-print("-" * (column_width * 3))
+print("-" * (column_width * 4))
 
 for resource in list(resource_list):
     if resource.type == 'Microsoft.Compute/virtualMachines':
-        print(f"{resource.name:<{column_width}}{resource.type:<{column_width}}"
-          f"{str(resource.created_time):<{column_width}}")
-#  f"{str(resource.created_time):<{column_width}}{str(resource.changed_time):<{column_width}}")
+        # Get vm status
+        vm = compute_client.virtual_machines.get(resource_group, resource.name, expand='instanceView')
 
-print("=" * (column_width * 3))
+        # These are the statuses of the VM about the event execution status and the vm state, the vm state is the second one.
+        vm_statuses = vm.instance_view.statuses
+        # print(vm_statuses[1].display_status)
+
+        print(f"{resource.name:<{column_width}}{resource.type:<{column_width}}"
+          f"{str(resource.created_time):<{column_width}}{str(vm_statuses[1].display_status):<{column_width}}")
+#  f"{str(resource.created_time):<{column_width}}{str(vm_statuses[1].display_status):<{column_width}}")
+
+print("=" * (column_width * 4))
 print('')
